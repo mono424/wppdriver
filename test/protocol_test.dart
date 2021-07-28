8e7b1d1d84d4291974424584136d156db73febc1
Khadim <khadimfall0602@gmail.com>
2021-07-28 23:56:49 +0200

    started a little with mock

--- test/protocol_test.dart
+++ test/protocol_test.dart
@@ -1,10 +1,27 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:wppdriver/WPPCommunicationClient.dart';
+import 'package:wppdriver/WPPDriver.dart';
 
+class MockWPPCommunicationClient extends Mock implements WPPCommunicationClient {}
+
+@GenerateMocks([WPPCommunicationClient])
 void main() {
   group('Handshake', () {
-    test('.split() splits the string on the delimiter', () {
-      var string = 'foo,bar,baz';
-      expect(string.split(','), equals(['foo', 'bar', 'baz']));
+    
+    test('valid handshake request is sent by driver', () {
+      final client = MockWPPCommunicationClient();
+      when(client.receiveStream).thenAnswer((_) => new Stream.empty());
+
+      final message = [
+        ..."H0010023".codeUnits, 0xA
+      ];
+
+      final driver = WPPDriver();
+      driver.init(client, 35);
+
+      verify(client.send(message));
     });
 
     test('.trim() removes surrounding whitespace', () {
